c6734d81b3b00c67f4f4b5d9070554b5908b43df
Swapnilthakare86 <sthakare@xtsworld.in>
2026-04-07 15:13:15 +0530

    change origin

--- Backend/app.js
+++ Backend/app.js
@@ -25,7 +25,7 @@ const dashboardRoutes = require("./routes/dashboard.routes");
 const errorHandler = require("./middleware/error.middleware");
 
 app.use(cors({
-  origin: 'http://13.204.174.119',
+  origin: 'http://13.232.32.154',
   credentials: true
 }));
 app.use(express.json());
